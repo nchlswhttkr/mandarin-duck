6bcca2c49c00f1d20a08008a5d893fd3c46ac222
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-06-05 00:57:30 +1000

    Keep test artifacts local to project
    Rather than writing to /tmp, keep test artifacts local. This fixes a bug
    when testing locally on macOS, where /tmp is an alias of /private/tmp.
    
    As an added benefit, parallel builds on the same machine are now
    possible in CI.

--- test.sh
+++ test.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 set -uo pipefail
 
-rm -rf /tmp/mandarin-duck
+rm -rf tests/workspaces/
 
 # Show last command for test output diffs
 git config diff.trace.xfuncname "^\+ .*$"
 
-find tests -name "*.test" | while read -r TEST; do
+find tests -maxdepth 1 -name "*.test" | while read -r TEST; do
     # Each test should use a namespaced area for its files
-    export WORKSPACE="/tmp/mandarin-duck/$TEST"
+    export WORKSPACE="$PWD/tests/workspaces/${TEST#tests/}"
     mkdir -p "$WORKSPACE"
 
     # By default, install directory should always be within workspace
     export DESTINATION="$WORKSPACE/mandarin-duck"
     export SKIP_BUILDKITE_API_CALLS_BECAUSE_TESTING=true
 
-    "./$TEST" 2>&1 | perl -pe 's/\x1b\[[0-9;]*[mG]//g' > "${TEST%.test}.trace"
+    "./$TEST" 2>&1 | perl -pe 's/\x1b\[[0-9;]*[mG]//g' | sed "s;$PWD;.;g" > "${TEST%.test}.trace"
     if git diff --exit-code "${TEST%.test}.trace" >/dev/null; then
         echo -e "--- \033[32m$TEST\033[0m"
     else
