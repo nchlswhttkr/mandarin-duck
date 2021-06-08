9c801b0fafb50ac96210139f601cef43d822d445
Nicholas Whittaker <nicholas@nicholas.cloud>
2021-06-08 20:12:24 +1000

    Fix unbound variable in post-receive script

--- post-receive.sh
+++ post-receive.sh
@@ -35,7 +35,7 @@ grep --invert-match " 0* " | cut -d " " -f 3 | grep "refs/heads/" | cut -c 12- |
         continue
     fi
     echo -e "--- Triggering Buildkite build on \033[32m$BRANCH\033[0m"
-    if [[ $SKIP_BUILDKITE_API_CALLS_BECAUSE_TESTING != "true" ]]; then
+    if [[ ${SKIP_BUILDKITE_API_CALLS_BECAUSE_TESTING:=''} != "true" ]]; then
         echo -n "You can view this build at "
         curl --fail --silent -X POST "https://api.buildkite.com/v2/organizations/$BUILDKITE_ORGANIZATION_SLUG/pipelines/$BUILDKITE_PIPELINE_SLUG/builds" \
             -H "Authorization: Bearer $BUILDKITE_API_TOKEN" \
